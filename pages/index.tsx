c1b04bbe0c6038eab77713f698166b663484a48d
Entreprenerd <valeranijr@gmail.com>
2021-04-15 18:47:34 +0200

    feat: UX is acceptable

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,67 +1,65 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { utils } from "ethers";
+import Head from "next/head";
+import { FormEvent, useState } from "react";
+import styles from "../styles/Home.module.css";
+import getTokenURI from "../utils/getTokenURI";
 
-const Home:React.FC =() => {
+// https://stackoverflow.com/questions/20169217/how-to-write-isnumber-in-javascript
+const isNumber = function isNumber(value) {
+  return typeof value === "number" && Number.isFinite(value);
+};
+
+const Home: React.FC = () => {
+  const [address, setAddress] = useState("");
+  const [tokenId, setTokenId] = useState("");
+  const [tokenUri, setTokenUri] = useState("");
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    if (!utils.isAddress(address)) {
+      console.log("???");
+      return alert("Address is not valid");
+    }
+
+    if (!isNumber(Number(tokenId))) {
+      return alert("TokenID must be a number");
+    }
+    const res = await getTokenURI(address, tokenId);
+    setTokenUri(res);
+  };
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Check Token Metadata</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="Address">
+            Address
+            <input
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+          </label>
+          <label htmlFor="TokenId">
+            TokenID
+            <input
+              value={tokenId}
+              onChange={(e) => setTokenId(e.target.value)}
+            />
+          </label>
+          <button type="submit">Find out</button>
+        </form>
+        {tokenId && (
+          <div>
+            <h3>Result:</h3>
+            <pre>{tokenUri}</pre>
+          </div>
+        )}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
